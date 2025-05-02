7bb9979f3f41a8f22f0b73d84cd9ecd1758a336b
Mark1-15 <markmarsden04@gmail.com>
2025-05-02 15:14:13 +0100

    added heroku procfile

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-53x_t1(9qcxr#n-xykvx&1nh&*eyjob*-*3$4opt8oo@^b9#_@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
